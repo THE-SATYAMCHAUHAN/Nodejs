54d72e35ef20259330556aa275b6f3cafe99ad50
THE-SATYAMCHAUHAN <110241083+THE-SATYAMCHAUHAN@users.noreply.github.com>
2024-01-07 02:04:09 +0530

    arrow function

--- hello.js
+++ hello.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+let add = (a,b)=>{
+    return a+b;
+}
+console.log(add(2,3));
+
+let student ={
+    name:'raj',
+    class: 5,
+    RollNo:15,
+    greet:function(){
+        console.log('my name is'+' '+this.name)
+    }
+}
+student.greet();
