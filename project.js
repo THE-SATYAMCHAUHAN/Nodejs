6580d92868c3d198faef4a46a502bd5498d41e38
THE-SATYAMCHAUHAN <110241083+THE-SATYAMCHAUHAN@users.noreply.github.com>
2024-01-22 23:58:22 +0530

    Add files via upload

--- project.js
+++ project.js
@@ -1,8 +1,20 @@
 const http=require('http');
 
-const routes=require('./route.js')
-const server = http.createServer(routes);
+const express =require('express');
 
-server.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+const app =express();
+
+app.use((req,res ,next)=>{
+  console.log('In the middleware');
+  next();
 });
+
+app.use((req,res ,next)=>{
+  console.log('In the middleware');
+  res.send('<h1>Hello From express!</h1>');
+});
+
+
+const server = http.createServer(app);
+
+server.listen(3000);
