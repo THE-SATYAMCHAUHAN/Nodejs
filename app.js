a89b1466fe86ae278207e6f151fa9807116ce865
THE-SATYAMCHAUHAN <110241083+THE-SATYAMCHAUHAN@users.noreply.github.com>
2024-01-30 02:36:37 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
+app.use(express.static('public'));
 
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  // Log the request details
-  console.log(req.url, req.method, req.headers);
-
-  // Set the response header
-  res.setHeader('Content-Type', 'text/html');
-
-  // Check the URL and send custom responses
-  if (req.url === '/home') {
-    res.write('<html><body><h1>Welcome home</h1></body></html>');
-  } else if (req.url === '/about') {
-    res.write('<html><body><h1>Welcome to About Us page</h1></body></html>');
-  } else if (req.url === '/node') {
-    res.write('<html><body><h1>Welcome to my Node Js project</h1></body></html>');
-  } else {
-    // Default response for other URLs
-    res.write('<html><body><h1>Hello from my Node.js server!</h1></body></html>');
-  }
-
-  // End the response
-  res.end();
+app.get('/', (req, res) => {
+  const messages = getMessages();
+  res.send(`
+    <h1>Messages</h1>
+    <form action="/send" method="post">
+      <label for="message">Message:</label>
+      <input type="text" id="message" name="message">
+      <button type="submit">Send</button>
+    </form>
+    <ul>
+      ${messages.map(message => `<li>${message}</li>`).join('')}
+    </ul>
+  `);
 });
 
-// Listen on port 4000
-server.listen(4000);
+app.get('/login', (req, res) => {
+  res.send(`
+    <h1>Login</h1>
+    <form action="/login" method="post">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username">
+      <button type="submit">Login</button>
+    </form>
+  `);
+});
 
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  if (username) {
+    res.cookie('username', username, { maxAge: 900000, httpOnly: true });
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/send', (req, res) => {
+  const username = req.cookies.username;
+  const message = req.body.message;
+  if (username && message) {
+    addMessage(username, message);
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+function getMessages() {
+  try {
+    const data = fs.readFileSync('messages.json');
+    return JSON.parse(data);
+  } catch (e) {
+    return [];
+  }
+}
+
+function addMessage(username, message) {
+  const messages = getMessages();
+  messages.push({ username, message });
+  fs.writeFileSync('messages.json', JSON.stringify(messages));
+}
+
+app.listen(3000, () => {
+  console.log('Server is running on port 4000');
+});
