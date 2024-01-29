dffccf3b318a84ac450a053479c4b2eb3709ae8e
THE-SATYAMCHAUHAN <110241083+THE-SATYAMCHAUHAN@users.noreply.github.com>
2024-01-30 04:06:37 +0530

    add contact us

--- project.js
+++ project.js
@@ -1,18 +1,21 @@
-const express =require('express');
-const bodyParser=require('body-parser');
+const path = require('path');
 
-const app =express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const adminRoutes =require('./routes2/admin');
-const shopRoutes=require('./routes2/Shop')
+const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+const adminRoutes = require('./routes2/admin');
+const shopRoutes = require('./routes2/shop');
 
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
